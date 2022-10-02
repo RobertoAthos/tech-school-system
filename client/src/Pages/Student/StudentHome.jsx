87993206ee3a375bc253c2c822b9e4545df4a40e
Roberto Athos <robertoathos22@gmail.com>
2022-10-02 14:37:06 -0300

    added event list and calendar in student interface

--- client/src/Pages/Student/StudentHome.jsx
+++ client/src/Pages/Student/StudentHome.jsx
@@ -1,13 +1,24 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import HomeHelper from "../../Components/HomeHelper";
 import "./StudentHome.css";
-import { AiOutlineWechat } from 'react-icons/ai'
-import { MdUpdate } from 'react-icons/md'
+import Calendar from "react-calendar";
+
 const Home = () => {
-  const store = useSelector((store) => store);
   const history = useHistory();
+  const store = useSelector((store) => store);
+  const dispatch = useDispatch();
+
+  const {
+    isLoadingPost,
+    successPost,
+    errorPost,
+    isLoading,
+    events,
+    error,
+    isLoadingDelete,
+  } = useSelector((state) => state.events);
 
   return (
     <section className="faculty-interface">
@@ -30,6 +41,28 @@ const Home = () => {
               
             </div>
           </div>
+          <div className="features-container">
+              <div className="events-container">
+                <div className="events">
+                  <h2>Eventos da escola</h2>
+                  {isLoading && <h2>Carregando...</h2>}
+
+                  {events.length === 0 ? (
+                    <h3>Nenhum evento encontrado</h3>
+                  ) : (
+                    events.events.map((event, index) => (
+                      <div key={index} className="tasks-card">
+                        <h2>{event.title}</h2>
+                        <p>{event.description}</p>
+                      </div>
+                    ))
+                  )}
+                </div>
+              </div>
+              <div className="calendar">
+                      <Calendar/>
+              </div>
+            </div>
         </div>
       </>
     ) : (
